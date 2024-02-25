8992aad1b655a9de82e62d0f08d5fb4eb859e1fd
Austin Schey <aschey13@gmail.com>
2024-02-25 11:43:30 -0600

    fix cleanup

--- crates/rooibos-dom/examples/counter_without_macros.rs
+++ crates/rooibos-dom/examples/counter_without_macros.rs
@@ -3,6 +3,7 @@ use std::error::Error;
 use std::fmt::format;
 use std::io::{stdout, Stdout};
 use std::rc::Rc;
+use std::sync::atomic::{AtomicU32, Ordering};
 use std::time::Duration;
 
 use crossterm::event::{self, Event, KeyCode};
@@ -10,7 +11,7 @@ use crossterm::execute;
 use crossterm::terminal::{
     disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen,
 };
-use leptos_reactive::{create_runtime, RwSignal, SignalGet, SignalUpdate};
+use leptos_reactive::{create_runtime, on_cleanup, RwSignal, SignalGet, SignalUpdate};
 use ratatui::backend::CrosstermBackend;
 use ratatui::layout::Constraint;
 use ratatui::Frame;
@@ -22,8 +23,10 @@ use rooibos_dom::{
 type Terminal = ratatui::Terminal<CrosstermBackend<Stdout>>;
 type Result<T> = std::result::Result<T, Box<dyn Error>>;
 
+static NODE_ID: AtomicU32 = AtomicU32::new(1);
+
 thread_local! {
-    static KEY_HANDLERS: RefCell<Vec<Rc<dyn Fn(String)>>> = RefCell::new(vec![]);
+    static KEY_HANDLERS: RefCell<Vec<Rc<(u32,Box<dyn Fn(String)>)>>> = RefCell::new(vec![]);
 }
 
 fn main() -> Result<()> {
@@ -57,7 +60,7 @@ fn handle_events() -> Result<usize> {
             }
             if let KeyCode::Char(c) = key.code {
                 let handlers = KEY_HANDLERS.with(|h| (*h.borrow()).clone());
-                handlers.iter().for_each(|h| (h)(c.to_string()));
+                handlers.iter().for_each(|h| (h.1)(c.to_string()));
 
                 return Ok(1);
             }
@@ -83,13 +86,24 @@ fn restore_terminal(mut terminal: Terminal) -> Result<()> {
 
 fn counter(initial_value: i32, step: u32) -> impl IntoView {
     let count = RwSignal::new(Count::new(initial_value, step));
-
+    let cur_id = NODE_ID.fetch_add(1, Ordering::Relaxed);
     KEY_HANDLERS.with(|h| {
-        h.borrow_mut().push(Rc::new(move |key| {
-            if key == "o" {
-                count.update(Count::increase);
-            }
-        }))
+        h.borrow_mut().push(Rc::new((
+            cur_id,
+            Box::new(move |key| {
+                if key == "o" {
+                    count.update(Count::increase);
+                }
+            }),
+        )));
+    });
+
+    on_cleanup(move || {
+        KEY_HANDLERS.with(|h| {
+            let mut h = h.borrow_mut();
+            let handler_pos = h.iter().position(|r| r.0 == cur_id).unwrap();
+            h.remove(handler_pos);
+        });
     });
 
     block(move || BlockProps::default().title(format!("count: {}", count.get().value())))
@@ -97,13 +111,16 @@ fn counter(initial_value: i32, step: u32) -> impl IntoView {
 
 fn counters() -> impl IntoView {
     let count = RwSignal::new(Count::new(2, 1));
-
+    let cur_id = NODE_ID.fetch_add(1, Ordering::Relaxed);
     KEY_HANDLERS.with(|h| {
-        h.borrow_mut().push(Rc::new(move |key| {
-            if key == "i" {
-                count.update(Count::increase);
-            }
-        }))
+        h.borrow_mut().push(Rc::new((
+            cur_id,
+            Box::new(move |key| {
+                if key == "i" {
+                    count.update(Count::increase);
+                }
+            }),
+        )));
     });
     col(Constraint::Percentage(100))
         .child(col(Constraint::Percentage(50)).child(move || {
