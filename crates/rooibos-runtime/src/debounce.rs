a19564221e17d4534b4e74641a353acfd571dbf8
Austin Schey <aschey13@gmail.com>
2024-08-22 20:22:32 -0700

    cancel debouncer task

--- crates/rooibos-runtime/src/debounce.rs
+++ crates/rooibos-runtime/src/debounce.rs
@@ -3,6 +3,8 @@ use std::sync::Arc;
 use std::time::Duration;
 
 use tokio::sync::mpsc;
+use tokio_util::sync::CancellationToken;
+use tracing::error;
 
 use crate::wasm_compat;
 
@@ -11,6 +13,13 @@ pub(crate) struct Debouncer<T> {
     update_tx: mpsc::Sender<()>,
     ready_rx: mpsc::Receiver<()>,
     has_value: Arc<AtomicBool>,
+    cancellation_token: CancellationToken,
+}
+
+impl<T> Drop for Debouncer<T> {
+    fn drop(&mut self) {
+        self.cancellation_token.cancel();
+    }
 }
 
 impl<T> Debouncer<T> {
@@ -18,9 +27,12 @@ impl<T> Debouncer<T> {
         let (ready_tx, ready_rx) = mpsc::channel(32);
         let (update_tx, mut update_rx) = mpsc::channel(32);
         let has_value = Arc::new(AtomicBool::new(false));
+        let cancellation_token = CancellationToken::new();
 
         wasm_compat::spawn_local({
             let has_value = has_value.clone();
+            let cancellation_token = cancellation_token.clone();
+
             async move {
                 loop {
                     let timeout = wasm_compat::sleep(debounce_time);
@@ -28,8 +40,14 @@ impl<T> Debouncer<T> {
                         _ = update_rx.recv() => {},
                         _ = timeout => {
                             if has_value.load(Ordering::Relaxed) {
-                                ready_tx.send(()).await.unwrap();
+                                let _ = ready_tx
+                                    .send(())
+                                    .await
+                                    .inspect_err(|e| error!("error sending ready signal: {e:?}"));
                             }
+                        }
+                        _ = cancellation_token.cancelled() => {
+                            return;
                         }
                     }
                 }
@@ -41,17 +59,24 @@ impl<T> Debouncer<T> {
             update_tx,
             ready_rx,
             has_value,
+            cancellation_token,
         }
     }
 
     pub(crate) async fn update(&mut self, value: T) {
         self.pending = Some(value);
         self.has_value.store(true, Ordering::Relaxed);
-        self.update_tx.send(()).await.unwrap();
+        self.update_tx
+            .send(())
+            .await
+            .expect("debouncer task cancelled");
     }
 
     pub(crate) async fn next_value(&mut self) -> T {
-        self.ready_rx.recv().await.unwrap();
+        self.ready_rx
+            .recv()
+            .await
+            .expect("debouncer task cancelled");
         self.has_value.store(false, Ordering::Relaxed);
         self.pending.take().unwrap()
     }
