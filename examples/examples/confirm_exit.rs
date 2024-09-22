1ae9e5e899af55af5d6c7f1a6d1d1d3d9627e803
Austin Schey <aschey13@gmail.com>
2024-09-22 16:21:43 -0700

    update examples

--- examples/examples/confirm_exit.rs
+++ examples/examples/confirm_exit.rs
@@ -1,9 +1,12 @@
-use rooibos::components::Show;
-use rooibos::dom::{KeyCode, focus_id, line};
-use rooibos::reactive::graph::signal::RwSignal;
+use rooibos::components::Button;
+use rooibos::dom::{KeyCode, focus_id, text};
+use rooibos::reactive::graph::effect::Effect;
+use rooibos::reactive::graph::signal::{ReadSignal, signal};
 use rooibos::reactive::graph::traits::{Get, Set};
-use rooibos::reactive::layout::{align_items, justify_content, position};
-use rooibos::reactive::{NodeId, Render, after_render, col, height, mount, row, wgt, width};
+use rooibos::reactive::layout::{align_items, chars, justify_content, position, show};
+use rooibos::reactive::{
+    NodeId, Render, UpdateLayoutProps, after_render, col, height, mount, wgt, width,
+};
 use rooibos::runtime::error::RuntimeError;
 use rooibos::runtime::{ExitResult, Runtime, before_exit, exit};
 use rooibos::terminal::crossterm::CrosstermBackend;
@@ -21,57 +24,73 @@ async fn main() -> Result<()> {
 }
 
 fn app() -> impl Render {
-    let show_popup = RwSignal::new(false);
-    let quit_confirmed = RwSignal::new(false);
+    let (show_popup, set_show_popup) = signal(false);
+    let (quit_confirmed, set_quit_confirmed) = signal(false);
 
     before_exit(move || async move {
         if quit_confirmed.get() {
             return ExitResult::Exit;
         }
-        show_popup.set(true);
+        set_show_popup.set(true);
         ExitResult::PreventExit
     });
 
     col![
-        wgt!(
-            Paragraph::new(vec![
-                line!("text1"),
-                line!("text2"),
-                line!("text3"),
-                line!("text4")
-            ])
-            .block(Block::bordered())
-        ),
-        row![
-            props(
-                width!(100.%),
-                height!(100.%),
-                position(Position::Absolute),
-                align_items(AlignItems::Center),
-                justify_content(JustifyContent::Center),
-            ),
-            Show::new().render(show_popup, move || {
-                let popup_id = NodeId::new_auto();
-                {
-                    after_render(move || {
-                        focus_id(popup_id);
-                    });
-                    wgt!(
-                        props(height!(3.), width!(40.)),
-                        Paragraph::new("Are you sure you want to exit? [yN]")
-                            .block(Block::bordered())
-                    )
-                    .id(popup_id)
-                    .on_key_down(move |key_event, _, _| {
-                        if key_event.code == KeyCode::Char('y') {
-                            quit_confirmed.set(true);
-                            exit();
-                        } else {
-                            show_popup.set(false);
-                        }
-                    })
-                }
+        Button::new()
+            .height(chars(3.))
+            .width(chars(8.))
+            .on_click(move || {
+                exit();
             })
-        ],
+            .render(text!("exit")),
+        popup(
+            show_popup,
+            move || {
+                set_quit_confirmed.set(true);
+                exit();
+            },
+            move || {
+                set_show_popup.set(false);
+            }
+        )
+    ]
+}
+
+fn popup(
+    show_popup: ReadSignal<bool>,
+    on_confirm: impl Fn() + 'static,
+    on_cancel: impl Fn() + 'static,
+) -> impl Render {
+    let popup_id = NodeId::new_auto();
+
+    Effect::new(move || {
+        if show_popup.get() {
+            after_render(move || {
+                focus_id(popup_id);
+            });
+        };
+    });
+
+    col![
+        props(
+            width!(100.%),
+            height!(100.%),
+            show(show_popup),
+            position(Position::Absolute),
+            align_items(AlignItems::Center),
+            justify_content(JustifyContent::Center),
+        ),
+        wgt!(
+            props(height!(3.), width!(40.)),
+            Paragraph::new("Are you sure you want to exit? [yN]").block(Block::bordered())
+        )
+        .id(popup_id)
+        .on_key_down(move |key_event, _, _| {
+            if key_event.code == KeyCode::Char('y') {
+                on_confirm();
+            } else {
+                on_cancel();
+            }
+        })
     ]
 }
